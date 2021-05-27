5f703504b8cb7fe09a81a4a5a04369f40f45f4de
nikolayshuklin <nikolayshuklin@mail.ru>
2021-05-27 09:53:01 +0300

    Initial commit

---
+++ app/src/main/java/ru/geekbrains/hellogeekbrains/MainActivity.java
@@ -0,0 +1,15 @@
+package ru.geekbrains.hellogeekbrains;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+}
