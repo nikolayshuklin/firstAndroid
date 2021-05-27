92f14c06caf0c69ccd33941ce2c95af196eba72a
nikolayshuklin <nikolayshuklin@mail.ru>
2021-05-27 17:52:42 +0300

    Создать ещё один макет (если не получается, то проект) с несколькими EditText, где использовать атрибут inputType: text, textPersonName, phone, number, textPassword, textEmailAddress и другие значения. Добавить в проект элемент CalendarView. * Разобраться, что такое параметр ems(это ширина буквы M в заданном размере шрифта)

--- app/src/main/java/ru/geekbrains/hellogeekbrains/MainActivity.java
+++ app/src/main/java/ru/geekbrains/hellogeekbrains/MainActivity.java
@@ -1,15 +1,38 @@
 package ru.geekbrains.hellogeekbrains;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        CalendarView calendarView = findViewById(R.id.calendarView);
+        Calendar calendar = Calendar.getInstance();
 
+        calendarView.setDate(calendar.getTimeInMillis());
 
-
+        setContentView(R.layout.activity_task2);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
